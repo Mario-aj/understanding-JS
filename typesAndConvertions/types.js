0fa2dc5ab3ec84afbe825e8f72331dfb7d1ed10e
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-04-05 20:38:57 +0300

    types in JS

---
+++ typesAndConvertions/types.js
@@ -0,0 +1,27 @@
+let firstName = 'Mario';
+console.log(typeof(firstName)); 
+
+let age = 23;
+console.log(typeof(age));
+
+let avarageOfAge = 12.5;
+console.log(typeof(avarageOfAge));
+
+let nullValue = null;
+console.log(typeof(nullValue));
+
+let undefinedValue = undefined;
+console.log(typeof(undefinedValue));
+
+let character = '@';
+console.log(typeof(character));
+
+let countries = ['Angola', 'Brazil', 'German', 'United State of America'];
+console.log(typeof(countries));
+
+let person = {
+  fullName: 'Mario Jorge',
+  age: 23,
+  nationality: 'Angola'
+};
+console.log(typeof(person));
