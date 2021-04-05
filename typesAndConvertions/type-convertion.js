58b452dabe01219821ab441b8c776f08a06ce0e6
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-04-05 20:49:39 +0300

    basic of type  convertion

---
+++ typesAndConvertions/type-convertion.js
@@ -0,0 +1,18 @@
+let answer = 60;
+console.log(answer);
+
+answer = 'Now, answer is a string'
+console.log(answer);
+
+// No JS, quando usamos o operador + entre string e number, o js converte tudo numa string só.
+let response = 'reponse data is ' + 60;
+console.log(response);
+
+response = 32 + ' is the response';
+console.log(response);
+
+// Com outros operadore o JS não converte valor numerico em string.
+console.log('43' - 21); // 22
+console.log('43' * 21); // 903
+console.log('43' / 21); // 2.047...
+console.log('43' + 21); // 4321
