e0271d2bf6903740fed4e86e76ece85bfb144c68
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-04-06 19:22:28 +0300

    explicit convertion

---
+++ typesAndConvertions/explicitConvertion.js
@@ -0,0 +1,16 @@
+/*
+* A forma mais comum de fazer conversões explicitas em JS, é usar as funções
+* Boolean()
+* String()
+* Number()
+* Object()
+*/
+
+console.log(Number("34")); //33
+
+console.log(String(false));
+
+console.log(Boolean(57));
+
+console.log(Object(1));
+
