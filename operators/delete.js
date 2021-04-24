628c4e4824b429cefef87dbd628d2aca5c82462f
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-04-24 13:16:45 +0300

    work with delete

---
+++ operators/delete.js
@@ -0,0 +1,28 @@
+let firstVariable = 'Mario';
+
+console.log(firstVariable)
+delete firstVariable;
+console.log(firstVariable);
+
+let person = {
+  name: 'Alfredo',
+  age: 23,
+  gender: 'male'
+};
+
+console.log(person.name);
+delete person.name;
+console.log(person.name);
+
+
+console.log(person);
+delete person;
+console.log(person);
+
+
+let people = ['Mario', 'Foguinho', 'Fernando', 'Isabel', 'Jorgito'];
+
+console.log(people.length);
+delete people[0];
+console.log(people.length);
+console.log(people[0]);
