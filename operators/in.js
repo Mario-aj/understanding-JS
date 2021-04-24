b6bbc2053e5a7cf96d8b60e3009c22816158be09
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-04-24 14:57:50 +0300

    work with in operator

--- operators/in.js
+++ operators/in.js
@@ -3,3 +3,7 @@ let country = ['Angola', 'Brazil', 'Russia'];
 console.log(1 in country);
 
 console.log('Angola' in country);
+
+var meucarro = {marca: "Honda", modelo: "Accord", ano: 1998};
+
+console.log("marca" in meucarro);
