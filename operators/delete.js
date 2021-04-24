c0fa7deea931a81ae5a42bd57d23fac6aecc7dec
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-04-24 13:50:08 +0300

    work with in

--- operators/delete.js
+++ operators/delete.js
@@ -14,11 +14,9 @@ console.log(person.name);
 delete person.name;
 console.log(person.name);
 
-
 console.log(person);
 delete person;
 console.log(person);
-
 
 let people = ['Mario', 'Foguinho', 'Fernando', 'Isabel', 'Jorgito'];
 
@@ -27,4 +25,4 @@ delete people[0];
 console.log(people.length);
 console.log(people[0]);
 
-console.log('Foguinho' in people);
+console.log(1 in people);
