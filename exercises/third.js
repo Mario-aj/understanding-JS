6757c7a11679f67624ecdd6a8c02a1abe2c4741a
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-04-25 23:13:10 +0300

    third exercise

---
+++ exercises/third.js
@@ -0,0 +1,26 @@
+/**
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ * What is the largest prime factor of the number 600851475143 ?
+ */
+
+let number = 600851475143;
+let counter = 2
+let divResult;
+let largestPrimeFactor = 0;
+
+while (counter <= number) {
+  divResult = number / counter;
+
+  let restCounter = 0;
+  for (i = 0; i <= divResult; i++) {
+    if(divResult % i === 0) restCounter++;
+  }
+
+  if (restCounter === 2)
+    if (divResult >= largestPrimeFactor)
+      largestPrimeFactor = divResult;
+
+  counter++;
+};
+
+console.log(largestPrimeFactor);
