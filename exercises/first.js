dd61be4a774f77a1605e49469688f4e101896ad9
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-04-25 11:00:23 +0300

    find all multiple sum of 3 and 5 below 1000

--- exercises/first.js
+++ exercises/first.js
@@ -1,13 +1,19 @@
+/**
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5,
+ * we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+
 let result = 0;
 let newResult = 0;
 
-for(let i = 0; i < 100; i++){
+for(let i = 0; i < 1000; i++){
   (i % 3 === 0 || i % 5 === 0) && (result += i);  
 };
 
 console.log(result);
 
-for (let j = 0;  j < 100; j++){
+for (let j = 0;  j < 1000; j++){
   if (j % 3 !== 0 && j % 5 !== 0)
     continue;
   newResult += j;
@@ -17,7 +23,7 @@ console.log(newResult);
 
 let resultWhile = 0;
 let count = 0
-while(count < 100){
+while(count < 1000){
   (count % 3 === 0 || count % 5 === 0) ? resultWhile += count++ : count++;
 }
 
