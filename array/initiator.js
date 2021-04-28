12585981ee74913bdeee50df3fac88e8dfaca4c0
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-04-28 13:04:14 +0300

    start with array in js

---
+++ array/initiator.js
@@ -0,0 +1,8 @@
+let arrayWithNewArray = new Array('elem0', 'elem1', 'elem2');
+let arrayWithArray = Array('elem0', 'elem1', 'elem2');
+let array = ['elem0', 'elem1', 'elem2'];
+
+
+console.log('Elements: ', arrayWithNewArray, 'length: ', arrayWithNewArray.length);
+console.log('\nElements: ', arrayWithArray, 'length: ', arrayWithArray.length);
+console.log('\nElements: ', array, 'length: ', array.length);
