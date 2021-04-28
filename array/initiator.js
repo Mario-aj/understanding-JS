22fdb46eddeaf064772f6192e78492b6aa9eb83d
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-04-28 13:12:11 +0300

    create an array with length different of zero

--- array/initiator.js
+++ array/initiator.js
@@ -1,8 +1,22 @@
+//Declarate an array.
 let arrayWithNewArray = new Array('elem0', 'elem1', 'elem2');
 let arrayWithArray = Array('elem0', 'elem1', 'elem2');
 let array = ['elem0', 'elem1', 'elem2'];
 
 
 console.log('Elements: ', arrayWithNewArray, 'length: ', arrayWithNewArray.length);
-console.log('\nElements: ', arrayWithArray, 'length: ', arrayWithArray.length);
-console.log('\nElements: ', array, 'length: ', array.length);
+console.log('Elements: ', arrayWithArray, 'length: ', arrayWithArray.length);
+console.log('Elements: ', array, 'length: ', array.length);
+
+//create array with length different of 0 and without elements. 
+let lengthOfArray = 32;
+let emptyArrayWithNewArray = new Array(lengthOfArray);
+let emptyArrayWithArray = Array(lengthOfArray);
+let emptyArray = [];
+
+emptyArray.length = lengthOfArray;
+
+console.log('length: ', emptyArrayWithNewArray);
+console.log('length: ', emptyArrayWithArray);
+console.log('length: ', emptyArray);
+
