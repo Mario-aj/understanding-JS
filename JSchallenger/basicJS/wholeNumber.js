6c22c1f69f702a924b874a5a20a838ccd3363ffe
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-04-30 12:41:04 +0300

    check if number is whole number

---
+++ JSchallenger/basicJS/wholeNumber.js
@@ -0,0 +1,15 @@
+/**
+ * Write a function that takes a number as argument
+ * If the number is a whole number (has no decimal place), return true
+ * Otherwise, return false
+ */
+
+function isWholeNumber(number) {
+  return Number.isInteger(number);
+};
+
+
+console.log(isWholeNumber(10));
+console.log(isWholeNumber(10.6));
+
+
