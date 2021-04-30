bb71ccde0872e6ab93c8dc29f77d0bdc217ad056
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-04-30 13:25:15 +0300

    concat string second method

--- JSchallenger/basicJS/concatString.js
+++ JSchallenger/basicJS/concatString.js
@@ -5,6 +5,7 @@
  * Return the concatenation
  */
 
+//function 1
 function concatStrings(string, valueToConcat) {
   let newString = '';
 
@@ -15,7 +16,20 @@ function concatStrings(string, valueToConcat) {
   return newString;
 }
 
+//function2
+function concatByAnotherMethods(str, value) {
+  return str.indexOf(value) >= 0
+    ? `${value}${str}`
+    : `${str}${value}`;
+}
+
 console.log(concatStrings("cheese", "cake"));
 console.log(concatStrings('lips', 's'));
 console.log(concatStrings("Java", "script"));
 console.log(concatStrings(" think, therefore I am","I"));
+
+console.log(concatByAnotherMethods("cheese", "cake"));
+console.log(concatByAnotherMethods('lips', 's'));
+console.log(concatByAnotherMethods("Java", "script"));
+console.log(concatByAnotherMethods(" think, therefore I am","I"));
+
