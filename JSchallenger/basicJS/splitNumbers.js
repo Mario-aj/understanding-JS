2f971cd4043a98962c6556177691ba1fb0b3abbe
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-05-02 23:06:00 +0300

    finesh the split numbers

--- JSchallenger/basicJS/splitNumbers.js
+++ JSchallenger/basicJS/splitNumbers.js
@@ -5,3 +5,11 @@
  */
 
 
+function splitNumbers(number) {
+  number = String(number).split('');
+  return number.map(value => Number(value));
+}
+
+console.log(splitNumbers(10));
+console.log(splitNumbers(937));
+console.log(splitNumbers(193278));
