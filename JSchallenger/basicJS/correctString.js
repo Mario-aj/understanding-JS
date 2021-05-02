d8d1bc91197e0f10ca0541670bcbf20eba266417
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-05-02 21:02:44 +0300

    uncrypted word

--- JSchallenger/basicJS/correctString.js
+++ JSchallenger/basicJS/correctString.js
@@ -5,6 +5,21 @@
  * Return the correct word
  */
 
-let str = 'a';
+function unencrypted(encryptedWord) {
+  const alphabet = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o','p','q', 'r', 's', 't', 'u', 'v', 'w', 'x','y', 'z'
+  ];
 
-console.log(str++);
+  encryptedWord = encryptedWord.split('');
+  let unencryptedWord = '';
+
+  for (let char of encryptedWord) {
+    let index = alphabet.indexOf(char);
+    unencryptedWord += alphabet[index + 1]; 
+  };
+
+  return unencryptedWord;
+}
+
+console.log(unencrypted('bnchmf'));
