2d763600869fbe1386121d147cdc150a6a87ed4a
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-05-02 23:43:18 +0300

    percentage of numbers

---
+++ JSchallenger/basicJS/percentageOfNumbers.js
@@ -0,0 +1,10 @@
+
+
+function percentageOfNumbers(prop) {
+
+  return prop.number * (prop.percentage / 100);
+}
+
+console.log(percentageOfNumbers({number: 100, percentage: 50}));
+console.log(percentageOfNumbers({number: 777, percentage: 2}));
+console.log(percentageOfNumbers({number: 500, percentage: 99}));
