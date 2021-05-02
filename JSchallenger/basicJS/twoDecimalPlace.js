dd2e6c4bff8264711ea81996d32251cd34dab925
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-05-02 19:22:14 +0300

    round number to the 2nd digit after the comma two methods

--- JSchallenger/basicJS/twoDecimalPlace.js
+++ JSchallenger/basicJS/twoDecimalPlace.js
@@ -6,11 +6,29 @@
 
 function roundedNumbers(number) {
 
-  return number.toFixed(2);
+  return Number(number.toFixed(2));
 };
 
 console.log(roundedNumbers(2.12397));
 console.log(roundedNumbers(3.136));
 console.log(roundedNumbers(1.12397));
 console.log(roundedNumbers(26.1379));
+
+
+function roundedNumbersMath(number) {
+  let [decimal, fractional] = String(number).split('.');
+  let counter = Number(fractional.length) - 2;
+
+  for (let i = 0; i < counter; i++)
+    fractional /= 10;
+
+  const roundedNumber = decimal + '.' + parseInt(fractional);
+
+  return Number(roundedNumber);
+}
+
+console.log(roundedNumbersMath(2.12397));
+console.log(roundedNumbersMath(3.136));
+console.log(roundedNumbersMath(1.12397));
+console.log(roundedNumbersMath(26.1379));
 
