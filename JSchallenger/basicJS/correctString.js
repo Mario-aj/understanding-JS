bb6c4e30bb268b5a9b35c9aa39ab633625a52330
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-05-02 20:00:58 +0300

    work with strings

---
+++ JSchallenger/basicJS/correctString.js
@@ -0,0 +1,10 @@
+/**
+ * Write a function that takes a string as argument
+ * As it is, the string has no meaning
+ * Increment each letter to the next letter in the alphabet
+ * Return the correct word
+ */
+
+let str = 'a';
+
+console.log(str++);
