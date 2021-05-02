458d39e0a53d0e97e5f1156c209829b8565f9a0b
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-05-02 19:51:21 +0300

    divisible

---
+++ JSchallenger/basicJS/disible.js
@@ -0,0 +1,21 @@
+/**
+ * // Write a function that takes two numbers, say x and y, as arguments
+ * Check if x is divisible by y
+ * If yes, return x
+ * If not, return the next higher natural number that is divisible by y
+ */
+
+function divisible(x, y) {
+  if (x % y === 0) return x;
+  else {
+    let xx = x; 
+    while(xx % y !== 0) xx++; 
+
+    return xx;
+  }
+};
+
+console.log(divisible(1, 23));
+console.log(divisible(23, 23));
+console.log(divisible(7, 3));
+console.log(divisible(-5, 7));
