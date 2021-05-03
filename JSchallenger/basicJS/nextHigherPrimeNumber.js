976b7be3c017d5947cc5d7cd5bf83c9dba86cf06
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-05-03 09:56:01 +0300

    solve the problem apply recursive concern

--- JSchallenger/basicJS/nextHigherPrimeNumber.js
+++ JSchallenger/basicJS/nextHigherPrimeNumber.js
@@ -5,9 +5,29 @@
  * If not, return the next higher prime number
  */
 
-function nextHigherPrimeNumber(number) {
+function findNextHigherPrimeNumber(number) {
+  let isDivisible = 0;
+  number++;
+  for (let i = 0; i <= number; i++)
+    if (number % i === 0) isDivisible++;
+  
+  if (isDivisible === 2) return number;
 
-  return number;
+  return findNextHigherPrimeNumber(number);
 };
 
-console.log(nextHigherPrimeNumber(38))
+function nextHigherPrimeNumber(number) {
+  let isDivisible = 0;
+  for (let i = 0; i <= number; i++)
+    if (number % i === 0) isDivisible++;
+  
+  if (isDivisible === 2) return number;
+
+
+  return findNextHigherPrimeNumber(number);
+};
+
+console.log(nextHigherPrimeNumber(38));
+console.log(nextHigherPrimeNumber(7));
+console.log(nextHigherPrimeNumber(115));
+console.log(nextHigherPrimeNumber(2000));
