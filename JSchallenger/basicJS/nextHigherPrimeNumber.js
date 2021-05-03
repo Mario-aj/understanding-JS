77bd2af7b57dc400c59e8dec20c55bbb91c94cf2
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-05-03 09:45:52 +0300

    next higher prime number

---
+++ JSchallenger/basicJS/nextHigherPrimeNumber.js
@@ -0,0 +1,13 @@
+/**
+ * This challenge is a little bit more complex
+ * Write a function that takes a number as argument
+ * If the number is prime, return the number
+ * If not, return the next higher prime number
+ */
+
+function nextHigherPrimeNumber(number) {
+
+  return number;
+};
+
+console.log(nextHigherPrimeNumber(38))
