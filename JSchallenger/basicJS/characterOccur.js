a36ce0f3f0da6d306f138fb4fe5c074e7d6c2cf2
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-05-03 09:29:58 +0300

    new exercise, times character occur

---
+++ JSchallenger/basicJS/characterOccur.js
@@ -0,0 +1,17 @@
+/**
+ * Write a function that takes a two strings as arguments
+ * Return the number of times the first string occurs in the second string
+ */
+
+function timesCharacterOccur(char, string) {
+
+
+  return true;
+};
+
+
+console.log(
+  timesCharacterOccur(
+    'm', 'How many times does the character occur in this sentence?'
+    )
+  );
