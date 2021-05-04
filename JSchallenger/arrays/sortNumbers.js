1be9619bfe0932bd55a615579c831e08eb1a6c61
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-05-04 09:26:00 +0300

    learn more about sort with numbers

--- JSchallenger/arrays/sortNumbers.js
+++ JSchallenger/arrays/sortNumbers.js
@@ -7,6 +7,6 @@ function SortNumbers(numbers) {
   return numbers.sort((a, b) => a > b ? -1 : 1);
 }
 
-console.log(SortNumbers([2, 5, 1]));
+console.log(SortNumbers([2, 5, 1, 2]));
 console.log(SortNumbers([1,3,2]));
 console.log(SortNumbers([4,2,3,1]));
