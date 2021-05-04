7edde7bc2a0f4587f8a1c24643daf28200364e3c
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-05-04 10:13:10 +0300

    return  last N array elements

---
+++ JSchallenger/arrays/returnLastNarrayElem.js
@@ -0,0 +1,16 @@
+/**
+ * Write a function that takes an array and a number (n) as arguments
+ * It should return the last n array elements
+ * If the array has less than n elements, return all
+ */
+
+
+function returnLastNArrayElements(array, n) {
+
+  return array.slice(array[array.length - n - 1], array.length);
+}
+
+console.log(returnLastNArrayElements([1, 2, 3, 4, 5], 2));
+console.log(returnLastNArrayElements([1, 2, 3], 6));
+console.log(returnLastNArrayElements([1, 2, 3, 4, 5, 6, 7, 8], 3));
+
