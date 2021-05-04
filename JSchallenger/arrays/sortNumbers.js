fb41c54cb7c512b6a777926e13b9c7de8ca35173
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-05-04 09:18:25 +0300

    sort numbers

---
+++ JSchallenger/arrays/sortNumbers.js
@@ -0,0 +1,12 @@
+/**
+ * Write a function that takes an array of numbers as argument
+ * It should return an array with the numbers sorted in descending order
+ */
+
+function SortNumbers(numbers) {
+  return numbers.sort((a, b) => a > b ? -1 : 1);
+}
+
+console.log(SortNumbers([2, 5, 1]));
+console.log(SortNumbers([1,3,2]));
+console.log(SortNumbers([4,2,3,1]));
