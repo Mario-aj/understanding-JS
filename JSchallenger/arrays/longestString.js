37c4e473205d466d08938005f0b463e1fc488387
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-05-05 17:31:23 +0300

    algothim that take the longest string

--- JSchallenger/arrays/longestString.js
+++ JSchallenger/arrays/longestString.js
@@ -4,9 +4,16 @@
  */
 
 function longestString(array) {
-
-  return;
+  let result = array[0];
+  for (let i = 1; i < array.length; i++)
+    if (array[i].length > result.length)
+      result = array[i];
+  
+  return result;
 }
 
 console.log(longestString(['help', 'me']));
 console.log(longestString(['I', 'need', 'candy']));
+console.log(longestString(['Mario', 'Alfredo', 'Jorge', 'Angola', 'Elineth']));
+console.log(longestString(['Fernando', 'Paulo', 'Isabel', 'Foguinho', 'Jorgito', 'Admira']));
+console.log(longestString(['Admira', 'Fernando', 'Paulo', 'Web developer']));
