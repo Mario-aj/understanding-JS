cbf16045c602a962342a630b063782ecf7457041
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-05-05 19:52:42 +0300

    there is another bug that i don't understand

--- JSchallenger/arrays/mergeAndRemoveDuplicatedValues.js
+++ JSchallenger/arrays/mergeAndRemoveDuplicatedValues.js
@@ -8,8 +8,8 @@
 function some(array) {
   for (let i = 0; i < array.length - 1; i++) {
     for (let j = i + 1; j < array.length; j++) 
-      if (array[i] === array[j]);
-      array.splice(i, 1);
+      if (array[i] === array[j])
+        array.splice(i, 1);
   };
   return array;
 };
@@ -23,3 +23,4 @@ function mergeAndRemoveDuplicatedValues(...arrays) {
 
 console.log(mergeAndRemoveDuplicatedValues([1, 2, 3], [3, 4, 5, , 6]));
 console.log(mergeAndRemoveDuplicatedValues([-10, 22, 333, 42], [-11, 5, 22, 41, 42]));
+console.log(mergeAndRemoveDuplicatedValues([1, 4, 7, 1, 2, 2, 1, 7, 4, 6], [3, 6, 7, 7, 1, 1, 4, 9, 6, 10, 56, 4, 2]));
