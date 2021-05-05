e517973910755e83722095fa997db45a55517f6d
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-05-05 16:46:58 +0300

    find the longest string in the array

---
+++ JSchallenger/arrays/longestString.js
@@ -0,0 +1,12 @@
+/**
+ * Write a function that takes an array of strings as argument
+ * It should return the longest string
+ */
+
+function longestString(array) {
+
+  return;
+}
+
+console.log(longestString(['help', 'me']));
+console.log(longestString(['I', 'need', 'candy']));
