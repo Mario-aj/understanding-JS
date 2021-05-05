9898a73852b857fc897a52574e6f124cc271e02f
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-05-05 17:50:45 +0300

    use js native function to find longest string into an array

--- JSchallenger/arrays/longestString.js
+++ JSchallenger/arrays/longestString.js
@@ -10,10 +10,21 @@ function longestString(array) {
       result = array[i];
   
   return result;
-}
+};
 
 console.log(longestString(['help', 'me']));
 console.log(longestString(['I', 'need', 'candy']));
 console.log(longestString(['Mario', 'Alfredo', 'Jorge', 'Angola', 'Elineth']));
 console.log(longestString(['Fernando', 'Paulo', 'Isabel', 'Foguinho', 'Jorgito', 'Admira']));
 console.log(longestString(['Admira', 'Fernando', 'Paulo', 'Web developer']));
+
+function longestStringWithReduce(array) {
+  return array.reduce((result, elem) => result.length > elem.length ? result : elem, '');
+};
+
+
+console.log(longestStringWithReduce(['help', 'me']));
+console.log(longestStringWithReduce(['I', 'need', 'candy']));
+console.log(longestStringWithReduce(['Mario', 'Alfredo', 'Jorge', 'Angola', 'Elineth']));
+console.log(longestStringWithReduce(['Fernando', 'Paulo', 'Isabel', 'Foguinho', 'Jorgito', 'Admira']));
+console.log(longestStringWithReduce(['Admira', 'Fernando', 'Paulo', 'Web developer']));
