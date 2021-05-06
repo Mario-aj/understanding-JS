749d631de8f480f8e087c3ecdfce4b802e7bbc28
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-05-06 10:14:26 +0300

    learn more about function

---
+++ functions/index.js
@@ -0,0 +1,13 @@
+/**
+ *    sintaxe
+ * function nameOfFunction() {
+ *  code
+ *  return valueOfReturn (optional)
+ * }
+ */
+ 
+function square(number) {
+  return number ** 2;
+};
+
+console.log(square(4));
