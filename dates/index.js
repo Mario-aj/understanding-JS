b0ca2e2daa5bcd84650f63b8fdce27f3aa93596f
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-05-06 09:02:10 +0300

    first program about date in js

---
+++ dates/index.js
@@ -0,0 +1,13 @@
+// constructors
+/**
+ * new Date()
+ * new Date(value)
+ * new Date(dateString)
+ * new Date(year, month, hour, minute, second,milisecond);
+ */
+
+let myDate = Date();
+let myNewDate = new Date();
+
+console.log('my date: ', myDate);
+console.log('my new date: ', myNewDate);
