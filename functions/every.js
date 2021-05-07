7d2308a4e937406d720c0f770203467125e70eaf
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-05-07 19:40:12 +0300

    refacto factorial function

---
+++ functions/every.js
@@ -0,0 +1,15 @@
+let every = (func, array) => {
+  
+  for (let i = 0; i < array.length; i++)
+    if (!func(array[i])) return false
+
+  return true
+};
+
+let arr = [5, 6, 7, 8, 9];
+//false
+console.log(every(value => value >= 6, arr));
+console.log(arr.every(value => value >= 6));
+//true
+console.log(every(value => value >= 6, arr));
+console.log(arr.every(value => value >= 6));
