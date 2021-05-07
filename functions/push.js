dfc62b3997b30ddd9ba2e9eb7c4292f3dbab6698
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-05-07 18:34:24 +0300

    add element with push method, when there is not position

--- functions/push.js
+++ functions/push.js
@@ -1,6 +1,11 @@
 let push = (array, element, position) => {
   let result = [];
 
+  if (!position){ 
+    array.push(element);
+    return array;
+  }
+
   if (array.length <= position) {
     array[array.length] = element;
 
@@ -21,3 +26,4 @@ let array = [1, 4, 6, 2];
 console.log(array);
 console.log(push(array, 7, 2));
 console.log(push(array, 8, 3));
+console.log(push(array, 900));
