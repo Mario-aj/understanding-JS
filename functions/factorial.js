01142f47342498f9820e0896958a8eb0e5011889
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-05-07 18:53:28 +0300

    refacto factorial function

--- functions/factorial.js
+++ functions/factorial.js
@@ -1,19 +1,19 @@
 function factorial(number) {
-  return number === 0 ? 1 : number * factorial(number - 1)
+  return number < 2 ? 1 : number * factorial(number - 1)
 };
 
 console.log(factorial(5));
 
 // anonymous function
 let anonymousFactorial = function(number) {
-  return number === 0 ? 1 : number * anonymousFactorial(number - 1);
+  return number < 2 ? 1 : number * anonymousFactorial(number - 1);
 };
 
 console.log(anonymousFactorial(5));
 
 // arrow function
 let arrowFunctionFactorial = (number) => {
-  return number === 0 ? 1 : number * anonymousFactorial(number - 1);
+  return number < 2 ? 1 : number * anonymousFactorial(number - 1);
 };
 
 console.log(arrowFunctionFactorial(5));
