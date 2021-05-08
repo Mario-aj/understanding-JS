bd1d57a8543bb62c99e240aed36bc26cb99e8ea8
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-05-08 17:05:35 +0300

    understanding more about reduce

--- functions/reduce.js
+++ functions/reduce.js
@@ -10,3 +10,11 @@ console.log(reduce((acumulator, element) => acumulator + element, arr, 0));
 
 let names = ['mario', 'alfredo', 'jorge'];
 console.log(reduce((acumulator, element) => acumulator + ' ' + element, names, ''));
+
+let person = [
+  { name: 'Mario', dev: true },
+  { name: 'Samuca', dev: true },
+  { name: 'Elineth', dev: false },
+  { name: 'Isabel', dev: false },
+];
+console.log(reduce((acumulator, element) => acumulator + ' ' + element.name, names, ''));
