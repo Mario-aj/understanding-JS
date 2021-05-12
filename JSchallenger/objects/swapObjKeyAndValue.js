afd9f157bac705094a558412f7df2faeb317f6f8
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-05-12 22:19:39 +0300

    swap key and value of any obj

---
+++ JSchallenger/objects/swapObjKeyAndValue.js
@@ -0,0 +1,13 @@
+/**
+ * Write a function that takes an object as argument
+ * Somehow, the properties and keys of the object got mixed up
+ * Swap the Javascript object's key with its values and return the resulting object
+ */
+let swapKey = (obj) => {
+  
+  let result = {};
+
+  for (let key in obj) 
+    result[obj[key]] = key;
+  return  result;
+};
