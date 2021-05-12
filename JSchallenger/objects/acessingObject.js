2714016bb581565883c539fbb8b24e0c33a3290a
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-05-12 21:18:55 +0300

    learn more about obj property

---
+++ JSchallenger/objects/acessingObject.js
@@ -0,0 +1,12 @@
+/**
+ * Write a function that takes an object with two properties as argument
+ * It should return the value of the property with key 'prop-2'
+ * Tipp: you might want to use the square brackets property accessor
+ */
+
+let acessingObjectProperties = (obj) => {
+
+  return obj['prop-2'];
+}
+
+console.log(acessingObjectProperties({  one: 1,  'prop-2': 2}));
