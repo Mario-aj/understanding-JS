e15a0cf1ab1e2fb584b8ff2d9252875615ac78d2
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-05-12 21:27:52 +0300

    more about obj property

---
+++ JSchallenger/objects/acessingObjectPropertyOne.js
@@ -0,0 +1,10 @@
+/**
+ * Write a function that takes an object with two properties as argument
+ * It should return the value of the property with key country
+ */
+
+let acessingObjectPropertyOne = (obj) => {
+  return obj.country;
+};
+
+console.log(acessingObjectPropertyOne({continent: 'Asia',  country: 'Japan'}));
