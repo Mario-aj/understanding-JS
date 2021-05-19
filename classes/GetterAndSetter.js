925c73a0dfd0a0f7667e9f3d2a9b846b398bdfe7
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-05-19 07:41:18 +0100

    learn more about set and get in JS

--- classes/GetterAndSetter.js
+++ classes/GetterAndSetter.js
@@ -11,10 +11,23 @@ class Family {
   get mother () {
     return this._mother;
   };
+
+  set father (str) {
+    this._father = str;
+  };
+
+  set mother (str) {
+    this._mother = str
+  };
 };
 
-
 let family = new Family('Paulo', 'Admira');
+
+console.log(family.father);
+console.log(family.mother);
+
+family.father = 'Mario';
+family.mother = 'Elineth';
 
 console.log(family.father);
 console.log(family.mother);
