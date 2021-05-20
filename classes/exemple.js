d76d6e88e9d7fc4396212d3efcc6c165ace7993e
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-05-20 08:59:02 +0100

    about extends and super in js

--- classes/exemple.js
+++ classes/exemple.js
@@ -14,6 +14,16 @@ Person.prototype.greeting = function () {
   console.log("Hi, I'm ", this.name.first + ' ' + this.name.last);
 };
 
-let person = new Person('Mario', 'Jorge', 22, 'Male', 'Programming');
+class Teacher extends Person{
+  constructor (first, last, age, gender, interests, subject) {
+    super(first, last, age, gender, interests);
+
+    this.subject = subject;
+  };
+};
+
+let person = new Person('Mario', 'Jorge', 23, 'Male', 'Programming');
+let teacher = new Teacher('Elineth', 'Tavares', 25, 'Female', 'Law', 'Mekaup');
 
 person.greeting();
+teacher.greeting();
