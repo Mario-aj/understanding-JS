db613391b418ad805119981588ad7f599ec9d243
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-05-22 08:32:37 +0100

    more about throw and try catch

---
+++ error/month.js
@@ -0,0 +1,25 @@
+class Months {
+  constructor (month) {
+    this.months = [
+      'jan', 'feb', 'mar', 'apr', 'may', 'jun',
+      'jul', 'aug', 'sep', 'Out', 'Nov', 'Dez',
+    ];
+
+    this.month = month - 1;
+  };
+
+  getMonthName() {
+    if (this.months[this.month]) 
+      return this.months[this.month];
+    else throw 'The month that you specified does not exist.';
+  };
+};
+
+try {
+  let month = new Months(13);
+
+  console.log(month.getMonthName());
+} catch(e) {
+  console.log('meu erro: ', e);
+};
+
