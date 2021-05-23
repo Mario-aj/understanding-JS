a1987f9eb76a3f05aecda5d64aa5c6098cfdede7
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-05-23 20:24:23 +0100

    the new exemple about use error exception in JS

--- error/error.js
+++ error/error.js
@@ -4,3 +4,26 @@ try {
   console.log('name: ', e.name);
   console.log('message: ', e.message);
 };
+
+// another exemple.
+class UserTypeError extends Error {
+  constructor (message) {
+    super(message)
+    this.name = 'userError';
+  };
+};
+
+const playload = '{ "type": "PJF" }';
+
+try {
+  const { type } = JSON.parse(playload);
+
+  if (!type) throw new UserTypeError('The type of playload did not find');
+
+  if (type === 'PF' || type === 'PJ') {
+    console.log('User valided');
+  } else throw new UserTypeError('type of user is invalid');
+}catch (e) {
+  console.log(e.name);
+  console.log(e.message);
+};
