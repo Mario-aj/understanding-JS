0237cc372de3340f2fd1736da5fb52be68ad562e
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-06-02 11:08:11 +0100

    solved exercise

--- exercises/smallestMultiple.js
+++ exercises/smallestMultiple.js
@@ -5,3 +5,25 @@
  * What is the smallest positive number that is evenly divisible by all of 
  *the numbers from 1 to 20?
  */
+
+const smallestMultiple = () => {
+  number = 20;
+  limit = number;
+  results = [];
+  //  divided = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  while (true) {
+    for (let i = 1; i <= limit; i++) {
+      if (number % i !== 0) {
+        results = [];
+        break;
+      }
+
+      results.push(i);
+    }
+
+    if (results.length === limit) return number;
+    number++;
+  }
+};
+
+console.log(smallestMultiple());
