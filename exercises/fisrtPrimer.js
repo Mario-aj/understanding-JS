e30fff9d4e2032e2625e3e594a454a23f7416292
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-06-04 09:26:18 +0100

    method that find prime numbers

--- exercises/fisrtPrimer.js
+++ exercises/fisrtPrimer.js
@@ -4,3 +4,32 @@
 
  * What is the 10 001st prime number?
  */
+
+const firstPrime = (props) => {
+  let numbers = [];
+  let result = [];
+
+  for (let i = 2; i <= props; i++) {
+    numbers.push(i);
+  }
+
+  numbers.forEach((elem) => {
+    let count = 0;
+
+    for (let i = 1; i <= elem; i++) {
+      if (elem % i === 0) count++;
+    }
+
+    if (count === 2) {
+      numbers = numbers.filter((e) => e % elem !== 0);
+      result.push(elem);
+    }
+
+    if (result.length === 6) break;
+    count = 0;
+  });
+
+  return result;
+};
+
+console.log(firstPrime(1000));
