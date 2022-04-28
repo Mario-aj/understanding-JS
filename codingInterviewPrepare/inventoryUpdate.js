4c03939d92cb957a1143ae9bb4504724f0aae345
mario-aj <mariojorge1997@mail.ru>
2022-04-28 13:33:08 +0100

    chore(inventoryUpdate): add exercise description

--- codingInterviewPrepare/inventoryUpdate.js
+++ codingInterviewPrepare/inventoryUpdate.js
@@ -1,6 +1,15 @@
+/**
+ * Compare and update the inventory stored in a 2D array against a second 2D
+ * array of a fresh delivery. Update the current existing inventory item
+ * quantities (in arr1). If an item cannot be found, add the new item and
+ * quantity into the inventory array. The returned inventory array should be in
+ * alphabetical order by item.
+ */
+
 function inventoryUpdate(currentInv = [], newInv = []) {
   let result = [];
   let indexes = [];
+
   for (let i = 0; i < currentInv.length; i++) {
     let updated = undefined;
     for (let j = 0; j < newInv.length; j++) {
